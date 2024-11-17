57e747d331cc0ec0ec6f749f8eb92834729d28b5
Sphinx <152749888+z3w1sz@users.noreply.github.com>
2024-11-17 20:26:24 -0300

    Update googleLogin.js

--- frontend/src/helpers/googleLogin.js
+++ frontend/src/helpers/googleLogin.js
@@ -1,3 +1,3 @@
 export const handleGoogleLogin = async () => {
-  window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=897335680197-ej7vtn0rmai1d42hmhbd48t455hoiaiu.apps.googleusercontent.com&redirect_uri=http://localhost:5173/auth/callback&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile%20openid`;
+  window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=897335680197-ej7vtn0rmai1d42hmhbd48t455hoiaiu.apps.googleusercontent.com&redirect_uri=https://gya2.vercel.app/auth/callback&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile%20openid`;
 };
