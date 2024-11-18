19077e9b52150d9ea48312c9156e631ac0b36e55
Agustin <agustinfratte@gmail.com>
2024-11-18 15:10:15 -0300

    Update CheckoutButton.jsx

--- frontend/src/modules/Main/Cart/components/CheckoutButton.jsx
+++ frontend/src/modules/Main/Cart/components/CheckoutButton.jsx
@@ -5,23 +5,32 @@ import axios from "axios";
 
 initMercadoPago("APP_USR-254ae5f4-0ef9-4dbb-8f28-9826f79049de", {
   locale: "es-AR",
-}); // Reemplaza con tu Public Key
+});
 
 const CheckoutButton = ({ orderData }) => {
   const [preferenceId, setPreferenceId] = useState(null);
   const { productsUrl } = useBaseUrl();
 
+  // Usamos useEffect para crear la preferencia solo una vez
   useEffect(() => {
-    axios
-      .post(productsUrl + "/create_preference", orderData)
-      .then((response) => setPreferenceId(response.data.preference_id))
-      .catch((error) => console.error("Error al crear la preferencia:", error));
-  }, [orderData]);
+    if (orderData && !preferenceId) {
+      axios
+        .post(productsUrl + "/create_preference", orderData)
+        .then((response) => {
+          setPreferenceId(response.data.preference_id);
+        })
+        .catch((error) =>
+          console.error("Error al crear la preferencia:", error)
+        );
+    }
+  }, [orderData, preferenceId]); // Solo se ejecuta si orderData o preferenceId cambian
 
   return (
     <>
-      {preferenceId && (
+      {preferenceId ? (
         <Wallet initialization={{ preferenceId: preferenceId }} />
+      ) : (
+        <p>Generando preferencia...</p> // Puedes mostrar un mensaje de carga si lo deseas
       )}
     </>
   );
