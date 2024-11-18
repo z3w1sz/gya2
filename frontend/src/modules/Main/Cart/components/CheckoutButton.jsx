e6d28fca8730753f77ed4888ee033654e49a1c2a
Agustin <agustinfratte@gmail.com>
2024-11-18 15:23:24 -0300

    Update CheckoutButton.jsx

--- frontend/src/modules/Main/Cart/components/CheckoutButton.jsx
+++ frontend/src/modules/Main/Cart/components/CheckoutButton.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
-import { useBaseUrl } from "../../../../context/BaseUrlContext";
 import axios from "axios";
+import { useBaseUrl } from "../../../../context/BaseUrlContext";
 
 initMercadoPago("APP_USR-254ae5f4-0ef9-4dbb-8f28-9826f79049de", {
   locale: "es-AR",
@@ -9,45 +9,29 @@ initMercadoPago("APP_USR-254ae5f4-0ef9-4dbb-8f28-9826f79049de", {
 
 const CheckoutButton = ({ orderData }) => {
   const [preferenceId, setPreferenceId] = useState(null);
-  const [loading, setLoading] = useState(true); // Estado de carga
-  const [error, setError] = useState(null); // Estado de error
   const { productsUrl } = useBaseUrl();
 
-  // Asegurarse de que la preferencia solo se cree una vez
+  // Solo crear la preferencia una vez
   useEffect(() => {
-    if (orderData && !preferenceId) {
-      // Solo crea la preferencia si no existe ya
-      setLoading(true); // Inicia el estado de carga
+    if (!preferenceId && orderData) {
       axios
         .post(productsUrl + "/create_preference", orderData)
         .then((response) => {
-          setPreferenceId(response.data.preference_id);
-          setLoading(false); // Finaliza el estado de carga
+          setPreferenceId(response.data.preference_id); // Solo actualiza si se recibe un preferenceId
         })
         .catch((error) => {
-          setError("Error al crear la preferencia: " + error.message); // Establece el error
-          setLoading(false); // Finaliza el estado de carga
+          console.error("Error al crear la preferencia", error);
         });
     }
-  }, [orderData, preferenceId, productsUrl]); // Solo ejecuta si orderData o preferenceId cambian
+  }, [orderData, preferenceId, productsUrl]);
 
-  if (loading) {
-    return <p>Cargando preferencia de pago...</p>; // Muestra un mensaje de carga mientras se genera la preferencia
-  }
-
-  if (error) {
-    return <p>{error}</p>; // Muestra el error si no se puede crear la preferencia
+  if (!preferenceId) {
+    return <p>Cargando el botón de pago...</p>; // Mensaje mientras se genera la preferencia
   }
 
   return (
-    <>
-      {preferenceId ? (
-        // El Wallet solo se renderiza si ya se ha generado la preferencia
-        <Wallet initialization={{ preferenceId: preferenceId }} />
-      ) : (
-        <p>No se pudo generar el botón de pago. Intenta nuevamente.</p>
-      )}
-    </>
+    // Renderiza Wallet solo cuando preferenceId esté disponible
+    <Wallet initialization={{ preferenceId }} />
   );
 };
 
